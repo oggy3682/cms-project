2444b5e9980f94f680afcf4705e7004b772dea4e
oggy3682 <tariyonlannister3682@gmail.com>
2025-03-30 02:44:12 +0530

    Fixed CORS issue

--- frontend/cms-frontend/src/api.js
+++ frontend/cms-frontend/src/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://127.0.0.1:8000/api/";
+const API_BASE_URL = "https://cms-project-rknw.onrender.com/api/";
+
+
 
 // Create Axios instance
 const api = axios.create({
